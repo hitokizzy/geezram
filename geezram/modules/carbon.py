e59693f8761bf335fd08d59d82cad3b857c41090
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-11-19 01:08:36 +0700

    del

--- geezram/modules/carbon.py
+++ geezram/modules/carbon.py
@@ -5,23 +5,23 @@ from telethon import __version__ as s
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
 from geezram import pbot
-from geezram.modules.utils.errors import capture_err
-from geezram.modules.utils.functions import make_carbon
+#from geezram.modules.utils.errors import capture_err
+#from geezram.modules.utils.functions import make_carbon
 
 
-@pbot.on_message(filters.command("carbon"))
-@capture_err
-async def carbon_func(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
-    if not message.reply_to_message.text:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
-    m = await message.reply_text("`Preparing Carbon`")
-    carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("`Uploading`")
-    await pbot.send_photo(message.chat.id, carbon)
-    await m.delete()
-    carbon.close()
+#@pbot.on_message(filters.command("carbon"))
+#@capture_err
+#async def carbon_func(_, message):
+#    if not message.reply_to_message:
+#        return await message.reply_text("`Reply to a text message to make carbon.`")
+#    if not message.reply_to_message.text:
+#        return await message.reply_text("`Reply to a text message to make carbon.`")
+#    m = await message.reply_text("`Preparing Carbon`")
+#    carbon = await make_carbon(message.reply_to_message.text)
+#    await m.edit("`Uploading`")
+#    await pbot.send_photo(message.chat.id, carbon)
+#    await m.delete()
+#    carbon.close()
 
 
 IRAA = "https://telegra.ph/file/3b757c3986ec72f09096c.jpg"
